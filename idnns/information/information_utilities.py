fixed gradients bug + dealing with more data

idnns/information/information_utilities.py
@@ -1,7 +1,8 @@
 import numpy as np
+num = 1
 def KL(a, b):
     """Calculate the Kullback Leibler divergence between a and b """
-    D_KL = np.nansum(np.multiply(a, np.log(np.divide(a, b+np.spacing(1)))))
+    D_KL = np.nansum(np.multiply(a, np.log(np.divide(a, b+np.spacing(1)))), axis=1)
     return D_KL
 
 
@@ -27,7 +28,7 @@ def calc_information_1(probTgivenXs, PYgivenTs, PXs, PYs, PTs):
     ITX = Ht - Htx
     return ITX, IYT
 
-def calc_information(probTgivenXs, PYgivenTs, PXs, PYs, PTs, probTgivenXs_n, PYgivenTs_n, PXs_n, PTs_n, type):
+def calc_information(probTgivenXs, PYgivenTs, PXs, PYs, PTs):
     """Calculate the MI - I(X;T) and I(Y;T)"""
     #PTs = np.nansum(probTgivenXs*PXs, axis=1)
     t_indeces = np.nonzero(PTs)
@@ -36,9 +37,6 @@ def calc_information(probTgivenXs, PYgivenTs, PXs, PYs, PTs, probTgivenXs_n, PYg
     Hyt = - np.nansum(np.dot(PYgivenTs*np.log2(PYgivenTs+np.spacing(1)), PTs))
     Hy = np.nansum(-PYs * np.log2(PYs+np.spacing(1)))
 
-    Ht_n = np.nansum(-np.dot(PTs_n, np.log2(PTs_n+np.spacing(1))))
-    Htx_n = - np.nansum((np.dot(np.multiply(probTgivenXs_n, np.log2(probTgivenXs_n)), PXs_n)))
-    Hyt_n = - np.nansum(np.dot(PYgivenTs_n * np.log2(PYgivenTs_n + np.spacing(1)), PTs_n))
     IYT = Hy - Hyt
     ITX = Ht - Htx
 

idnns/information_network.py
@@ -7,6 +7,7 @@ import cPickle
 import shutil
 import re
 import argparse
+import sys
 from idnns.plots import plot_figures as plt_fig
 from idnns.information import information_process  as inn
 import  tensorflow as tf
@@ -103,7 +104,7 @@ def get_default_parser(num_of_samples = None):
                         help='The interval for display the information calculation')
 
     parser.add_argument('-cov_net',
-                        '-cov', dest="cov_net", type=str2bool, nargs='?', const=False,default=False,
+                        '-cov', dest="cov_net", type=int, default=0,
                         help='True if we want covnet')
 
     parser.add_argument('-rl',
@@ -153,8 +154,8 @@ class informationNetwork():
         #load data
         self.data_sets = nn.load_data(self.name, args.random_labels)
         #create arrays for saving the data
-        self.ws, self.grads, self.information,self.models ,self.names, self.networks = [[[[[None] for k in range(len(self.train_samples))] for j in range(len(self.layers_sizes))]
-                      for i in range(self.num_of_repeats)] for _ in range(6)]
+        self.ws, self.grads, self.information,self.models ,self.names, self.networks,self.weights = [[[[[None] for k in range(len(self.train_samples))] for j in range(len(self.layers_sizes))]
+                      for i in range(self.num_of_repeats)] for _ in range(7)]
 
         self.loss_train, self.loss_test,  self.test_error, self.train_error, self.l1_norms, self.l2_norms= \
             [np.zeros((self.num_of_repeats, len(self.layers_sizes), len(self.train_samples), len(self.epochs_indexes))) for _ in range(6)]
@@ -176,11 +177,12 @@ class informationNetwork():
 
     def save_data(self,parent_dir='jobs/', file_to_save = 'data.pickle'):
         """Save the data to the file """
-        directory =  '{0}{1}/'.format(parent_dir, self.params['directory'])
+        directory =  '{0}/{1}{2}/'.format(os.path.dirname(sys.argv[0]), parent_dir, self.params['directory'])
+
         data = {'information': self.information,
                      'test_error': self.test_error, 'train_error': self.train_error, 'var_grad_val': self.grads,
                      'loss_test': self.loss_test, 'loss_train': self.loss_train, 'params': self.params
-            , 'l1_norms': self.l1_norms, 'activation': self.ws}
+            , 'l1_norms': self.l1_norms, 'weights': self.weights, 'ws': self.ws}
 
         if not os.path.exists(directory):
             os.makedirs(directory)
@@ -206,7 +208,7 @@ class informationNetwork():
         elif type_net == '5':
             self.layers_sizes = [[10]]
         elif type_net == '6':
-            self.layers_sizes = [[1000, 500, 100]]
+            self.layers_sizes = [[1, 1, 1,1]]
         else:
             #Custom network
             self.layers_sizes = [map(int, inner.split(',')) for inner in re.findall("\[(.*?)\]", type_net)]
@@ -244,6 +246,7 @@ class informationNetwork():
                     current_network = results[index]
                     self.networks[k][j][i] = current_network
                     self.ws[k][j][i] =current_network['ws']
+                    self.weights[k][j][i] = current_network['weights']
                     self.information[k][j][i] = current_network['information']
                     self.grads[k][i][i] = current_network['gradients']
                     self.test_error[k, j, i, :] = current_network['test_prediction']

idnns/model.py
@@ -33,6 +33,47 @@ def _convert_string_dtype(dtype):
     else:
         raise ValueError('Unsupported dtype:', dtype)
 
+
+def multi_layer_perceptron(x, n_input, n_classes, n_hidden_1, n_hidden_2):
+    hidden = []
+    input = []
+    hidden.append(x)
+    # Network Parameters
+    #n_input = x.shape[0]  # MNIST data input (img shape: 28*28)
+    #n_classes = 10  # MNIST total classes (0-9 digits)
+
+    weights = {
+        'h1': tf.Variable(tf.random_normal([n_input, n_hidden_1])),
+        'h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2])),
+        'out': tf.Variable(tf.random_normal([n_hidden_2, n_classes]))
+    }
+    biases = {
+        'b1': tf.Variable(tf.random_normal([n_hidden_1])),
+        'b2': tf.Variable(tf.random_normal([n_hidden_2])),
+        'out': tf.Variable(tf.random_normal([n_classes]))
+    }
+
+    # Hidden layer with RELU activation
+    layer_1_input = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
+    layer_1 = tf.nn.relu(layer_1_input)
+    input.append(layer_1)
+    hidden.append(layer_1)
+    # Hidden layer with RELU activation
+    layer_2_input = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
+    layer_2 = tf.nn.relu(layer_2_input)
+    input.append(layer_2_input)
+    hidden.append(layer_2)
+    # Output layer with linear activation
+    input_y = tf.matmul(layer_2, weights['out']) + biases['out']
+    y_output = tf.nn.softmax(input_y)
+    input.append(y_output)
+    hidden.append(y_output)
+
+    return y_output,hidden, input
+
+
+
+
 def deepnn(x):
     """deepnn builds the graph for a deep net for classifying digits.
     Args:
@@ -200,6 +241,8 @@ class Model:
         self.savers = []
         if activation_function ==1:
             self.activation_function = tf.nn.relu
+        elif activation_function==2:
+            self.activation_function =None
         else:
             self.activation_function = tf.nn.tanh
         self.prediction
@@ -222,7 +265,10 @@ class Model:
         variable_summaries(biases)
         with tf.variable_scope(name_scope) as scope:
             input = tf.matmul(last_hidden, weights) + biases
-            output = activation_function(input, name='output')
+            if activation_function ==None:
+                output =input
+            else:
+                output = activation_function(input, name='output')
         self.inputs.append(input)
         self.hidden.append(output)
         return output
@@ -237,9 +283,12 @@ class Model:
         if self.hidden is None:
             self.hidden,self.inputs,self.weights_all,self.biases_all = [], [], [],[]
             last_hidden = self.x
-            if self.covnet:
+            if self.covnet==1:
                 y_conv, self._drouput, self.hidden,self.inputs = deepnn(self.x)
+            elif self.covnet==2:
+                y_c, self.hidden, self.inputs = multi_layer_perceptron(self.x, self.input_size, self.num_of_classes ,self.layerSize[0], self.layerSize[1])
             else:
+
                 self._drouput ='dr'
                 #self.hidden.append(self.x)
                 for i in xrange(1, len(self.all_layer_sizes)):
@@ -359,5 +408,10 @@ class Model:
         feed_dict = {self.x: X}
         layer_values = session.run(inputs, feed_dict=feed_dict)
         return layer_values
+
+    def print_model_prop(self):
+        """model properties -" 
+            Structure - {0}""".format(self.hidden)
+        print
 
 

idnns/network.py
@@ -85,7 +85,7 @@ def train_and_calc_inf_network(i,j,k,layerSize, num_of_ephocs, learning_rate_loc
     print ('Training network  - {0}'.format(network_name))
     network = train_network(layerSize, num_of_ephocs, learning_rate_local, batch_size, indexes, save_grads,
                   data_sets_org, model_type, percent_of_train, interval_accuracy_display, network_name, cov_net)
-
+    network['information'] = []
     if calc_information:
         print ('Calculating the infomration')
         infomration = np.array([inn.get_information(network['ws'], data_sets_org.data, data_sets_org.labels, network['estimted_label'],
@@ -108,7 +108,7 @@ def train_network(layerSize, num_of_ephocs, learning_rate_local, batch_size, ind
     """Train the nework"""
     tf.reset_default_graph()
     data_sets = data_shuffle(data_sets_org, percent_of_train)
-    ws, estimted_label,gradients, infomration, models= [[None]*len(indexes) for _ in range(5) ]
+    ws, estimted_label,gradients, infomration, models,weights= [[None]*len(indexes) for _ in range(6) ]
     loss_func_test, loss_func_train, test_prediction, train_prediction = [np.zeros((len(indexes))) for _ in range(4)]
     input_size = data_sets_org.data.shape[1]
     num_of_classes = data_sets_org.labels.shape[1]
@@ -116,11 +116,15 @@ def train_network(layerSize, num_of_ephocs, learning_rate_local, batch_size, ind
     save_file = 'net_' +name
     batch_size = np.min([batch_size,data_sets.train.data.shape[0]])
     batch_points = np.rint(np.arange(0, data_sets.train.data.shape[0]+1,batch_size)).astype(dtype=np.int32)
-    batch_points_test = np.rint(np.arange(0, data_sets_org.data.shape[0]+1,batch_size)).astype(dtype=np.int32)
-    if data_sets_org.data.shape[0] not in batch_points_test:
-        batch_points_test = np.append(batch_points_test, [data_sets_org.data.shape[0]])
+    batch_points_test = np.rint(np.arange(0, data_sets.test.data.shape[0]+1,batch_size)).astype(dtype=np.int32)
+    batch_points_all = np.rint(np.arange(0, data_sets_org.data.shape[0]+1,batch_size)).astype(dtype=np.int32)
+
+    if data_sets_org.data.shape[0] not in batch_points_all:
+        batch_points_all = np.append(batch_points_all, [data_sets_org.data.shape[0]])
     if data_sets.train.data.shape[0] not in batch_points:
         batch_points = np.append(batch_points, [data_sets.train.data.shape[0]])
+    if data_sets.test.data.shape[0] not in batch_points_test:
+        batch_points_test = np.append(batch_points_test, [data_sets.test.data.shape[0]])
     #Build the network
     model = bulid_model(model_type,layerSize, input_size, num_of_classes,learning_rate_local, save_file,covn_net)
     optimizer = model.optimize
@@ -140,20 +144,38 @@ def train_network(layerSize, num_of_ephocs, learning_rate_local, batch_size, ind
         sess.run(init)
         #Go over the epochs
         k =0
+        acc_train_array = 0
         for j in range(0, num_of_ephocs):
             epochs_grads = []
             if j in indexes:
-                ws[k], estimted_label[k] = \
-                sess.run([model.hidden_layers, model.prediction],
-                             feed_dict=feed_dict_all)
+                estimted_label[k] = 0
+                w_temp = []
+                #print len(batch_points_all)
+                for i in xrange(0, len(batch_points_all) - 1):
+                    batch_xs = data_sets_org.data[batch_points_all[i]:batch_points_all[i + 1]]
+                    batch_ys = data_sets_org.labels[batch_points_all[i]:batch_points_all[i + 1]]
+                    feed_dict_temp = {model.x: batch_xs, model.labels: batch_ys}
+                    if covn_net:
+                        feed_dict_temp[model.drouput] = 1
+                    w_temp_local = sess.run([model.hidden_layers],
+                                 feed_dict=feed_dict_temp)
+                    #print i
+                    for s in range(len(w_temp_local[0])):
+                        if i==0:
+                            w_temp.append(w_temp_local[0][s])
+                        else:
+                            w_temp[s] = np.concatenate((w_temp[s], w_temp_local[0][s]), axis=0)
+                #print ('iiiiiiiii', len(w_temp))
+                ws[k] = w_temp
+                """"
                 loss_func_train[k], train_prediction[k] = \
                     sess.run([ model.cross_entropy, model.accuracy],
                              feed_dict=feed_dict_train)
-
+                print j
                 loss_func_test[k], test_prediction[k] = \
                     sess.run([model.cross_entropy, model.accuracy],
                              feed_dict=feed_dict_test)
-                """"
+                
                 infomration[k] = inn.calc_information_for_epoch(k, interval_information_display, ws_t, params['bins'],
                                                   params['unique_inverse_x'],
                                                   params['unique_inverse_y'],
@@ -165,29 +187,56 @@ def train_network(layerSize, num_of_ephocs, learning_rate_local, batch_size, ind
                 """
             # Print accuracy every some epochs
             if np.mod(j, interval_accuracy_display) ==1 or interval_accuracy_display ==1:
-                print ('Epoch {0} - Test Accuracy: {1:.3f}, Train Accuracy: {2:.3f}'.format(j,sess.run(model.accuracy, feed_dict=feed_dict_test),
-                                                                                            sess.run(model.accuracy,
-                                                                                                     feed_dict=feed_dict_train)))
+                acc_array  =[]
+                for i in xrange(0, len(batch_points_test) - 1):
+                    batch_xs = data_sets.test.data[batch_points_test[i]:batch_points_test[i + 1]]
+                    batch_ys = data_sets.test.labels[batch_points_test[i]:batch_points_test[i + 1]]
+                    feed_dict_temp = {model.x: batch_xs, model.labels: batch_ys}
+                    if covn_net:
+                        feed_dict_temp[model.drouput] = 1
+                    acc = sess.run([model.accuracy],
+                                 feed_dict=feed_dict_temp)
+                    acc_array.append(acc)
+                print ('Epoch {0} - Test Accuracy: {1:.3f} Train Accuracy: {2:.3f}'.format(j,np.mean(np.array(acc_array)), np.mean(np.array(acc_train_array))))
+                #print ('Epoch {0} - Test Accuracy: {1:.3f}, Train Accuracy: {2:.3f}'.format(j,sess.run(model.accuracy, feed_dict=feed_dict_test),
+                #                                                                            sess.run(model.accuracy,
+                #                                                                                    feed_dict=feed_dict_train)))
             #Go over the batch_points
+            acc_train_array = []
+            current_weights =   [[] for _ in range( len(model.weights_all))]
             for i in xrange(0, len(batch_points) - 1):
+                #print i
                 batch_xs = data_sets.train.data[batch_points[i]:batch_points[i + 1]]
                 batch_ys = data_sets.train.labels[batch_points[i]:batch_points[i + 1]]
                 feed_dict = {model.x: batch_xs, model.labels: batch_ys}
                 if covn_net:
                     feed_dict[model.drouput] = 0.5
                 optimizer.run(feed_dict)
+
                 if j in indexes:
-                    epochs_grads_temp, loss_tr, tr_err = sess.run([grads, model.cross_entropy, model.accuracy],
+                    epochs_grads_temp, loss_tr, tr_err, weights_local = sess.run([grads, model.cross_entropy, model.accuracy, model.weights_all],
                                                              feed_dict=feed_dict)
                     epochs_grads.append(epochs_grads_temp)
+                    acc_train_array.append(tr_err)
+                    for ii in range(len(current_weights)):
+                        current_weights[ii].append(weights_local[ii])
+
             if j in indexes:
                 if save_grads:
                     gradients[k] = epochs_grads
+                    current_weights_mean  = []
+                    for ii in range(len(current_weights)):
+                        current_weights_mean.append(np.mean(np.array(current_weights[ii]), axis=0))
+                    weights[k] = current_weights_mean
                 #Save the model
                 write_meta = True if k == 0 else False
-                saver.save(sess, model.save_file, global_step=k, write_meta_graph=write_meta)
+                #saver.save(sess, model.save_file, global_step=k, write_meta_graph=write_meta)
                 k+=1
+            else:
+                acc_train_array  =0
     network = {}
+    network['weights'] = weights
+
     network['ws'] = ws
     network['estimted_label'] = estimted_label
     network['test_prediction'] = test_prediction